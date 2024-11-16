84fa6284666a524f0810bcc8591a8ce07beb50cb
Marcos Reyes <astronautmarkus@gmail.com>
2024-11-16 19:55:15 -0300

    'Cocinero views' templates, create 'components/dashboard_cards.css', changed buttons for a in 'Cocinero index'.

--- mofulunches-web/app/routes/cocineros_routes.py
+++ mofulunches-web/app/routes/cocineros_routes.py
@@ -9,4 +9,19 @@ def cocineros_index():
         error_message = "Acceso denegado. Debes ser cocinero para acceder a esta página."
         return render_template('error.html', error_message=error_message)
 
-    return render_template('cocineros/index.html', user=user)
+    return render_template('cocineros/cocineros-menu.html', user=user)
+
+
+@cocineros_bp.route('/cartas-menu')
+def cartas_menu():
+    return render_template('cocineros/cartas-menu.html')
+
+@cocineros_bp.route('/platillos-menu')
+def platillos_menu():
+    return render_template('cocineros/platillos-menu.html')
+
+@cocineros_bp.route('/ingredientes-menu')
+def ingredientes_menu():
+    return render_template('cocineros/ingredientes-menu.html')
+
+
